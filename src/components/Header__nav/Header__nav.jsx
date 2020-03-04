fd1dd43b11c94b1d67b3f8661a0eb17dc490acdc
Alex James <alexjames@Alexs-MacBook-Pro.local>
2020-03-04 14:32:55 -0600

    completed layout of header navigation

--- src/components/Header__nav/Header__nav.jsx
+++ src/components/Header__nav/Header__nav.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import classy from '../../utils/classy';
 import style from './Header__nav.module.scss';
 import { Route } from  'react-router';
-import slackLogo from './images/slack-logo-icon.png';
+import {Link} from 'react-router-dom';
 
 
 
@@ -10,6 +10,16 @@ export default class Header__nav extends React.Component {
     render() {
         return(
             <div>
+                <nav className={classy(style.navigation, style.navigationMain, style.nav)}>
+                    <ul className={classy(style.navigation__menuList, style.nav__menuList)}>
+                        <li className={style.nav__menuItem}>
+                            <Link to={"/"} className={style.nav__link}>Work</Link>
+                        </li>
+                        <li className={classy(style.nav__menuItem, style.nav__menuItemActive)}>
+                            <Link to={"/Contact"} className={style.nav__link}>Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         );
     }
